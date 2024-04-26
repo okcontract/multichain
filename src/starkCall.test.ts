bbfca245132455d035e1776817d4677f99465055
Henri Binsztok <808274+hbbio@users.noreply.github.com>
2024-04-26 15:42:01 +0800

    starkCall: fix test counter

--- src/starkCall.test.ts
+++ src/starkCall.test.ts
@@ -288,7 +288,7 @@ test("starknet multicall", async () => {
 
   // at least one call (there may be retries due to RPC errors/quotas/etc.)
   // @todo count retries and expect `.toBe(1+retries)`
-  expect(await multi._counter("starknet")).toBeGreaterThan(1);
+  expect(await multi._counter("starknet")).toBeGreaterThanOrEqual(1);
 
   const res1 = await uncellify(bal1);
   expect(decodeShortString(toHex(res1[0]))).toEqual("ETH");
