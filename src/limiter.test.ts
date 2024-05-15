7de5186562956ce9b65fba342cbcfeb6ba9b9fe7
Henri Binsztok <808274+hbbio@users.noreply.github.com>
2024-05-15 09:58:24 +0800

    limiter: test endpoints update

--- src/limiter.test.ts
+++ src/limiter.test.ts
@@ -7,11 +7,14 @@ import { RateLimiter } from "./limiter";
 test("RateLimiter", async () => {
   const sheet = new Sheet();
   const proxy = new SheetProxy(sheet);
-  const endpoints = proxy.new(["a", "b", "c"], "endpoints");
+  const endpoints = proxy.new(["a", "b"], "endpoints");
   const limiter = new RateLimiter(endpoints, 50);
   expect(limiter.take("a")).toBe(true);
   expect(limiter.take("a")).toBe(false);
   expect(limiter.take("b")).toBe(true);
   await sleep(60);
   expect(limiter.take("a")).toBe(true);
+  endpoints.update((l) => [...l, "c"]);
+  expect(limiter.take("c")).toBe(true);
+  expect(limiter.take("c")).toBe(false);
 });
